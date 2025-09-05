5b8ec26e158282fe5fde1c7f9564b0b40add16eb
fluffypal <fluffypal17@gmail.com>
2025-09-05 23:59:16 +0300

    added firewalld settings shell script

---
+++ Scripts/firewalld.sh
@@ -0,0 +1,30 @@
+echo "helloowww it is firewalld settings setup"
+echo "Firewalld Installed ?"
+read -p "y/n " choice
+choice=$(echo "$choice" | tr '[:upper:]' '[:lower:]')
+if [[ "$choice" == "y" || "$choice" == "yes" ]]; then
+  ip -br a
+  echo "what is yourr interface??"
+  read -p "enter pleasee: " interface
+  echo "defualt zone block and your $interface home zone."
+  echo "localsend app for 53317/tcp and 53317/udp opened"
+  read -p "y/n " twochoice
+  twochoice=$(echo "$twochoice" | tr '[:upper:]' '[:lower:]')
+  if [[ "$twochoice" == "y" || "$twochoice" == "yes" ]]; then
+    sudo firewall-cmd --set-default-zone=block
+    sudo firewall-cmd --remove-interface=$interface --zone=block
+    sudo firewall-cmd --add-interface=$interface --zone=home
+    sudo firewall-cmd --zone=home --add-port=53317/tcp --permanent
+    sudo firewall-cmd --zone=home --add-port=53317/udp --permanent
+    sudo firewall-cmd --runtime-to-permanent
+    sudo firewall-cmd --reload
+  else
+    exit 1
+  fi
+
+elif [[ "$choice" == "n" || "$choice" == "no" ]]; then
+  echo "okey tschüüss"
+else
+  echo "plsss enterrr y/n"
+  exit 1
+fi
