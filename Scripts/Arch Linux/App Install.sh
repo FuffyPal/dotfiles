9aa0c507f2864a2d5b15b424c5dac2aa43635192
fluffypal <fluffypal17@gmail.com>
2025-09-06 11:55:56 +0300

    added: App install.sh for archlinux non nvidia user

--- Scripts/Arch Linux/App Install.sh
+++ Scripts/Arch Linux/App Install.sh
@@ -1,3 +1,3 @@
-sudo pacman -S --noconfirm --needed bluez  bluez-utils flatpak git upower  helix noto-fonts noto-fonts-cjk noto-fonts-emoji  timeshift firewalld bash-completion  wireshark-qt qt6-multimedia-ffmpeg gst-plugin-pipewire power-profiles-daemon nvidia-settings nvidia-container-toolkit  podman python-pip rust-analyzer npm  nextcloud-client git-lfs fish
+sudo pacman -S --noconfirm --needed bluez  bluez-utils flatpak git upower  helix noto-fonts noto-fonts-cjk noto-fonts-emoji  timeshift firewalld bash-completion  wireshark-qt qt6-multimedia-ffmpeg gst-plugin-pipewire power-profiles-daemon podman python-pip rust-analyzer npm  nextcloud-client git-lfs fish
 yay -S --noconfirm steam-devices-git zapret-git lmstudio 
 
