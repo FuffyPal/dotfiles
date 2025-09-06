263a2778bbf493916f418def987696de78b611f8
fluffypal <fluffypal17@gmail.com>
2025-09-06 11:25:33 +0300

    added script miniconda.sh

---
+++ Scripts/miniconda.sh
@@ -0,0 +1,23 @@
+echo "heloowww miniconda intalled script welcome"
+echo "
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm ~/miniconda3/miniconda.sh
+~/miniconda3/bin/conda init --all
+"
+read -p "y/n " choice
+choice=$(echo "$choice" | tr '[:upper:]' '[:lower:]')
+if [[ "$choice" == "y" || "$choice" == "yes" ]]; then
+  mkdir -p ~/miniconda3
+  wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+  bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+  rm ~/miniconda3/miniconda.sh
+  ~/miniconda3/bin/conda init --all
+  exit 1
+elif [[ "$choice" == "n" || "$choice" == "no" ]]; then
+  echo "okeyyy tcshüss"
+  exit 1
+else
+  echo "pleaseee enteerrr y/n"
+fi
