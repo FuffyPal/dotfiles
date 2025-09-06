c24d86bf42e7c4e5a8354aa6a770cfe09f732f99
Fluffy Pal <fluffypal17@gmail.com>
2025-09-07 01:09:55 +0300

    added: kdeconnect ports in firewalld.sh config

--- Scripts/firewalld.sh
+++ Scripts/firewalld.sh
@@ -8,14 +8,17 @@ if [[ "$choice" == "y" || "$choice" == "yes" ]]; then
   read -p "enter pleasee: " interface
   echo "defualt zone block and your $interface home zone."
   echo "localsend app for 53317/tcp and 53317/udp opened"
+  echo "kdeconnect app for 1714 to 1764 and tcp/udp opened"
   read -p "y/n " twochoice
   twochoice=$(echo "$twochoice" | tr '[:upper:]' '[:lower:]')
   if [[ "$twochoice" == "y" || "$twochoice" == "yes" ]]; then
     sudo firewall-cmd --set-default-zone=block
     sudo firewall-cmd --remove-interface=$interface --zone=block
     sudo firewall-cmd --add-interface=$interface --zone=home
-    sudo firewall-cmd --zone=home --add-port=53317/tcp --permanent
-    sudo firewall-cmd --zone=home --add-port=53317/udp --permanent
+    sudo firewall-cmd --zone=home --add-port=53317/tcp
+    sudo firewall-cmd --zone=home --add-port=53317/udp 
+    sudo firewall-cmd --zone=home --add-port=1714-1764/tcp
+    sudo firewall-cmd --zone=home --add-port=1714-1764/udp
     sudo firewall-cmd --runtime-to-permanent
     sudo firewall-cmd --reload
   else
