46ea7d7e300eb2ea0ab44594005416ef5c29d16a
Fluffy Pal <fluffypal17@gmail.com>
2025-09-23 20:45:41 +0300

    hotfix: automatic yes remove in auto.sh yes command

--- Scripts/Fedora/Auto.sh
+++ Scripts/Fedora/Auto.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 ./Fedora_Install.sh
-yes | ../dns.sh
-../firewalld.sh
-yes | ../flatpak.sh
-yes | ../miniconda.sh
-../zapret.sh
-yes | ../langserver.sh
-../theme.sh
+cd ../
+./dns.sh
+./firewalld.sh
+yes | ./flatpak.sh
+yes | ./miniconda.sh
+./zapret.sh
+yes | ./langserver.sh
+./theme.sh
