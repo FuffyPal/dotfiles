77e6e2525fcef128e78e2d2838be5cf079325ad3
Fluffy Pal <fluffypal17@gmail.com>
2025-10-01 16:29:02 +0300

    added: mint package setting in package.sh

--- Scripts/Mint/Package.sh
+++ Scripts/Mint/Package.sh
@@ -1,9 +1,57 @@
-echo "script for linux mint cinnamon version"
+echo "script for linux mint cinnamon edi~"
 
-
-add_package="
-
-"
-remove_package="
+Add_repos="
 
 "
+
+Add_package="
+gnome-session
+gnome-shell
+gnome-core
+gnome-tweaks
+pipewire
+pipewire-pulse
+wireplumber
+"
+
+Remove_package="
+gnome-calculator
+gnome-calendar
+gnome-contacts
+gnome-sushi
+eog
+celluloid
+xreader*
+ufw
+libreoffice-*
+thunderbird*
+firefox*
+transmission*
+rhythmbox*
+onboard*
+pix*
+warpinator
+ubuntu-session
+evince
+simple-scan
+mintbackup
+mintinstall
+mintmenu
+mintstick
+mintwelcome
+mintupdate
+mint-upgrade-info
+hypnotix
+drawing
+xed
+gnome-shell-extensions
+gnome-shell-extension-prefs
+sticky
+cinnamon*
+lightdm*
+"
+
+Update_defualt="sudo apt update"
+Upgrade_defualt="sudo apt upgrade -y"
+Install_defualt="sudo apt install -y"
+Remove_defualt="sudo apt remove --purge --auto-remove -y"
